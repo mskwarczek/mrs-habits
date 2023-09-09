621683c4d810def154296a82aeca2c23fc3aea9d
Maciej Skwarczek <m.skwarczek@gmail.com>
2023-09-09 13:44:28 +0200

    chore: init CRA with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HabitsApp = () => {
+  return (
+    <>
+      <header>
+      </header>
+      <main>
+      </main>
+    </>
+  );
+};
+
+export default HabitsApp;
